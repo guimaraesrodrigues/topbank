24ee5f28a31167af255a7b7b5845c61f25d98bd8
Rodrigo <guimaraes_rodrigues@yahoo.com>
2017-06-06 12:26:14 -0300

    Primeira implementação do cliente e modelo para conta

--- server/models.py
+++ server/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Conta(models.Model):
+	nome_cliente = models.CharField('nome do cliente', max_length=100)
+	saldo = models.FloatField()
+
+	class Meta:
+		verbose_name = 'conta'
+		verbose_name_plural = 'contas'
+
+	def __str__(self):
+		return '{0.id} - {0.nome_cliente}'.format(self)
--- topbank/settings.py
+++ topbank/settings.py
@@ -31,7 +31,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.AllowAny'
     ]
 }
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'server',
 ]
 
 MIDDLEWARE = [
